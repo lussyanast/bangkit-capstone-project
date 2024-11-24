556fb1042a983558c9c1f3e9a3c021d0d350e121
lussyanast <lussyanast@gmail.com>
2024-11-24 11:42:32 +0700

    update

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,33 +1,33 @@
 // (Import fungsi-fungsi handler dari file handlers) (Work in progress)
-const { signin, signup, signout } = require("../handlers/authHandler");
+// const { signin, signup, signout } = require("../handlers/authHandler");
 
-const routes = (server, myModels) => {
-	server.route({
-		method: "POST",
-		path: "/signin",
-		handler: signin,
-		options: {
-			auth: false,
-		},
-	});
+// const routes = (server, myModels) => {
+	// server.route({
+	// 	method: "POST",
+	// 	path: "/signin",
+	// 	handler: signin,
+	// 	options: {
+	// 		auth: false,
+	// 	},
+	// });
 
-	server.route({
-		method: "POST",
-		path: "/signup",
-		handler: signup,
-		options: {
-			auth: false,
-		},
-	});
+	// server.route({
+	// 	method: "POST",
+	// 	path: "/signup",
+	// 	handler: signup,
+	// 	options: {
+	// 		auth: false,
+	// 	},
+	// });
 
-	server.route({
-		method: "POST",
-		path: "/signout",
-		handler: signout,
-		options: {
-			auth: "session",
-		},
-	});
-};
+	// server.route({
+	// 	method: "POST",
+	// 	path: "/signout",
+	// 	handler: signout,
+	// 	options: {
+	// 		auth: "session",
+	// 	},
+	// });
+// };
 
 module.exports = routes;
