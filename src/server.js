1dca9483cdaf82d628cef9c280d15d2280220a1d
Reinhad Tangguh Ariyanto <85008633+rta0227@users.noreply.github.com>
2024-12-06 09:26:41 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const routes = require("./routes/detectionRoutes");
 const init = async () => {
     const server = Hapi.server({
         port: 3000,
-        host: "localhost",
+        host: "0.0.0.0",
         routes: {
             cors: {
                 origin: ["*"], // Izinkan CORS
